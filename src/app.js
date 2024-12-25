66835bf217030672547be36b7d0f4c5b72753599
anantkpatel <imagine231216@gmail.com>
2024-12-25 11:33:22 +0530

    playing with routes

--- src/app.js
+++ src/app.js
@@ -3,15 +3,17 @@ const express= require("express");
 const app = express();
 
 // this will use all api call form this route it include all http methods 
-app.delete("/hello",(req,res)=>{
-    res.send("hello world from hellow page");
+app.delete("/delete",(req,res)=>{
+    res.send("delete to hellow world");
 })
 app.post("/test",(req,res)=>{
     res.send("this is test page");
 })
-app.get("/test",(req,res)=>{
-    res.send("this is test page");
-})
+app.get("/user/:userid/:name/:password",(req,res)=>{
+    console.log(req.params);
+    res.send("this is hello get");
+});
+
 app.use("/",(req,res)=>{
     res.send("hellow world main page");
 })
