09410f534a38edd86c76d2234ecf60fee1fa3846
anantkpatel <imagine231216@gmail.com>
2024-12-25 10:37:38 +0530

    exploring get,post patch,delete

--- src/app.js
+++ src/app.js
@@ -1,7 +1,18 @@
 console.log("Dev tider is running");
 const express= require("express");
 const app = express();
-app.use((req,res)=>{
-    res.send("hellow world");
+
+// this will use all api call form this route it include all http methods 
+app.delete("/hello",(req,res)=>{
+    res.send("hello world from hellow page");
+})
+app.post("/test",(req,res)=>{
+    res.send("this is test page");
+})
+app.get("/test",(req,res)=>{
+    res.send("this is test page");
+})
+app.use("/",(req,res)=>{
+    res.send("hellow world main page");
 })
 app.listen(3000);
