3af911af443a61ef6087340554145d7813ab8dd7
anantkpatel <imagine231216@gmail.com>
2025-09-12 12:36:25 +0530

    l

--- src/config/database.js
+++ src/config/database.js
@@ -3,7 +3,7 @@ const mongoose = require('mongoose');
 const connectDb = async () => 
 {
     await mongoose.connect(
-   "mongodb+srv://imagine231216_db_user:8ShbD0zYCBNUBQDU@cluster0.ktd7iqz.mongodb.net/devtinder"
+   "mongodb+srv://imagine231216_db_user:8ShbD0zYCBNUBQDU@cluster0.ktd7iqz.mongodb.net/test"
 
     );
 }
